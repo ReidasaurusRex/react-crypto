52b5068b3ab81a4e4ce0570bc12adf8e19bab773
ReidasaurusRex <reidhlewis91@gmail.com>
2017-01-04 11:17:01 -0800

    Added empty Enctypt component

---
+++ src/App/encrypt/Encrypt.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Encrypt extends Component {
+  render() {
+    return (
+      <div>
+        <p>Encrypt</p>
+      </div>
+    );
+  }
+}
+
+export default Encrypt;
