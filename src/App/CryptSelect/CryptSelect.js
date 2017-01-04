971306116aa5671aab903586a0dca870665f08f1
ReidasaurusRex <reidhlewis91@gmail.com>
2017-01-04 11:13:20 -0800

    Added empty CryptSelect Component

---
+++ src/App/CryptSelect/CryptSelect.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CryptSelect extends Component {
+  render() {
+    return (
+      <div>
+        <p>Crypt Select</p>
+      </div>
+    );
+  }
+}
+
+export default CryptSelect;
