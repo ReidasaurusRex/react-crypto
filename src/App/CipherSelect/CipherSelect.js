06c8b7defcf315af4c5790e68444486b1b8077a5
ReidasaurusRex <reidhlewis91@gmail.com>
2017-01-04 11:11:53 -0800

    Added empty CipherSelect React component

---
+++ src/App/CipherSelect/CipherSelect.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CipherSelect extends Component {
+  render() {
+    return (
+      <div>
+        <p>Cipher Select</p>
+      </div>
+    );
+  }
+}
+
+export default CipherSelect;
