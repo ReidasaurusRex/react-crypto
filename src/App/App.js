947140ebcce74e67ff07470757960d97f5740cd5
ReidasaurusRex <reidhlewis91@gmail.com>
2017-01-04 11:44:01 -0800

    Added intial components to App. Creating a separate Header component

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import CryptSelect from './CryptSelect/CryptSelect';
+import CipherSelect from './CipherSelect/CipherSelect';
+import Encrypt from './Encrypt/Encrypt';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div>
-          <h2>Cipher</h2>
-        </div>
+        
+        <header className='c-header'>
+        
+          <h2 className='c-header__title'>Cipher</h2>
+          
+          <nav className='c-header__selectors'>
+            <CryptSelect />
+            <CipherSelect />
+          </nav>
+
+        </header>
+
+        <Encrypt />
+
       </div>
     );
   }
