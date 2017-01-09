60388f803f3aadf633d502cb8f3060f6b91aebac
ReidasaurusRex <reidhlewis91@gmail.com>
2017-01-09 11:10:13 -0800

    Defined tests for a few corner cases of CryptHandler. Called caesarCrypt and vigenereCrypt in handleCrypt, both without functionality yet.

--- src/CryptHandler.js
+++ src/CryptHandler.js
@@ -1,20 +1,32 @@
 class CryptHandler {
   static handleCrypt(cipher, key, crypt, message) {
-    if (cipher === 'caesar') {
-
-    } else if (cipher === 'vigenere') {
-
+    if (crypt === 'encrypt' || crypt === 'decrypt') {
+      if (cipher === 'caesar') {
+        return this.caesarCrypt(key, crypt, message);
+      } else if (cipher === 'vigenere') {
+        return this.vigenereCrypt(key, crypt, message);
+      } else {
+        throw("Incorrect cipher passed to handleCrypt. Expected 'caesar' or 'vigenere', received " + cipher + ".");
+      }
     } else {
-      throw("Incorrect cipher. Expected 'caesar' or 'vigenere', received " + cipher)
+      throw("Incorrect crypt passed to handleCrypt. Expected 'encrypt' or 'decrypt', received " + crypt + ".");
     }
+    
   }
 
   static caesarCrypt(key, crypt, message) {
-
+    if (Number.isInteger(key)) {
+      let messageArray = message.split();
+      messageArray.forEach((c) => {
+        
+      })
+    } else {
+      throw("Incorrect data type passed to caesarCrypt. Expected an integer, Number.isInteger(" + key + ") returned false.");
+    }
   }
 
   static vigenereCrypt(key, crypt, message) {
-    
+
   }
 }
 
--- src/CryptHandler.test.js
+++ src/CryptHandler.test.js
@@ -1,13 +1,19 @@
 import CryptHandler from './CryptHandler';
 const message = 'Fish Sandwich!';
 
-// it('returns the same string with Caesar 0', () => {
-//   const caesarEncryption = CryptHandler.handleCrypt('caesar', 0, 'encrypt', message);
-//   expect(caesarEncryption).toBe("Fish Sandwich!");
-// });
+it('returns the same string with Caesar 0', () => {
+  const caesarEncryption = CryptHandler.handleCrypt('caesar', 0, 'encrypt', message);
+  expect(caesarEncryption).toBe("Fish Sandwich!");
+});
 
 it('throws when passed Fish as a cipher', () => {
   expect(() => {
     CryptHandler.handleCrypt('Fish', 0, 'encrypt', message);
   }).toThrow();
 });
+
+it('throws when passed Fish as a crypt', () => {
+  expect(() => {
+    CryptHandler.handleCrypt('caesar', 0, 'Fish', message);
+  }).toThrow();
+})
