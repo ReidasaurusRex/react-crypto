e8d41afb23f2f8538eb3ad4461456a07c3ae0729
ReidasaurusRex <reidhlewis91@gmail.com>
2017-01-10 10:10:51 -0800

    Fractured CryptHandler.test.js into multiple describes

--- src/CryptHandler.test.js
+++ src/CryptHandler.test.js
@@ -1,34 +1,39 @@
 import CryptHandler from './CryptHandler';
 const message = 'Fish Sandwich!';
 
-it('returns the same string with Caesar 0', () => {
-  const caesarEncryption = CryptHandler.handleCrypt('caesar', 0, 'encrypt', message);
-  expect(caesarEncryption).toBe("Fish Sandwich!");
+describe('cryptHandler', () => {
+  test('throws when passed Fish as a cipher', () => {
+    expect(() => {
+      CryptHandler.handleCrypt('Fish', 0, 'encrypt', message);
+    }).toThrow();
+  });
+
+  test('throws when passed Fish as a crypt', () => {
+    expect(() => {
+      CryptHandler.handleCrypt('caesar', 0, 'Fish', message);
+    }).toThrow();
+  });
 });
 
-it('encrypts Fish Sandwich to Gjti Tboexjdi! with Caesar 1', () => {
-  const caesarEncryption = CryptHandler.handleCrypt('caesar', 1, 'encrypt', message);
-  expect(caesarEncryption).toBe('Gjti Tboexjdi!');
-})
+describe('caesar', () => {
+  test('returns the same string with key 0', () => {
+    const caesarEncryption = CryptHandler.handleCrypt('caesar', 0, 'encrypt', message);
+    expect(caesarEncryption).toBe("Fish Sandwich!");
+  });
 
-it('decrypts Gjti Tboexjdi! to Fish Sandwich! with Caesar 1', () => {
-  const caesarEncryption = CryptHandler.handleCrypt('caesar', 1, 'decrypt', 'Gjti Tboexjdi!');
-  expect(caesarEncryption).toBe('Fish Sandwich!');
-})
+  test('encrypts Fish Sandwich to Gjti Tboexjdi! with key 1', () => {
+    const caesarEncryption = CryptHandler.handleCrypt('caesar', 1, 'encrypt', message);
+    expect(caesarEncryption).toBe('Gjti Tboexjdi!');
+  });
 
-it('properly bounces end of alphabet to beginning of alphabet with Caesar', () => {
-  const caesarEncryption = CryptHandler.handleCrypt('caesar', 1, 'encrypt', 'AAZZ az azz zz');
-  expect(caesarEncryption).toBe('BBAA ba baa aa');
-})
+  test('decrypts Gjti Tboexjdi! to Fish Sandwich! with key 1', () => {
+    const caesarEncryption = CryptHandler.handleCrypt('caesar', 1, 'decrypt', 'Gjti Tboexjdi!');
+    expect(caesarEncryption).toBe('Fish Sandwich!');
+  });
 
-it('throws when passed Fish as a cipher', () => {
-  expect(() => {
-    CryptHandler.handleCrypt('Fish', 0, 'encrypt', message);
-  }).toThrow();
+  test('properly bounces end of alphabet to beginning of alphabet', () => {
+    const caesarEncryption = CryptHandler.handleCrypt('caesar', 1, 'encrypt', 'AAZZ az azz zz');
+    expect(caesarEncryption).toBe('BBAA ba baa aa');
+  });
 });
 
-it('throws when passed Fish as a crypt', () => {
-  expect(() => {
-    CryptHandler.handleCrypt('caesar', 0, 'Fish', message);
-  }).toThrow();
-})
