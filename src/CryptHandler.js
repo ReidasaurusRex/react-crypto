0fbd35fd8317678d87c2b4458c24015946eeb466
ReidasaurusRex <reidhlewis91@gmail.com>
2017-01-11 13:14:21 -0800

    Fixed vigenereCrypt to properly handle decrypts.

--- src/CryptHandler.js
+++ src/CryptHandler.js
@@ -21,7 +21,7 @@ class CryptHandler {
         if ((message[i] >= 'A' && message[i] <= 'Z' ) || (message[i] >= 'a' && message[i] <= 'z')) {
           const reducer = (message[i] <= 'Z') ? 'A'.charCodeAt(0) : 'a'.charCodeAt(0);
           const reducedChar = message[i].charCodeAt(0) - reducer;
-          const keyedChar = (crypt === 'encrypt') ? (reducedChar + key) % 26 : (reducedChar - key) % 26;
+          const keyedChar = (crypt === 'encrypt') ? (reducedChar + key) % 26 : (reducedChar + (26 - key)) % 26;
           returnMessage += String.fromCharCode(keyedChar + reducer);
         } else {
           returnMessage += message[i];
@@ -29,12 +29,31 @@ class CryptHandler {
       }
       return returnMessage;
     } else {
-      throw("Incorrect data type passed to caesarCrypt. Expected an integer, Number.isInteger(" + key + ") returned false.");
+      throw("Incorrect key type passed to caesarCrypt. Expected an integer, Number.isInteger(" + key + ") returned false.");
     }
   }
 
   static vigenereCrypt(key, crypt, message) {
-
+    if (key.match(/[A-Za-z]+$/) && !key.includes(' ')) {
+      var returnMessage = '';
+      var keyCounter = 0; 
+      for (var i = 0; i < message.length; i++) {
+        if (message[i].match(/[A-za-z]/)) {
+          const keyCode = key.charCodeAt(keyCounter);
+          const keyShift = key[keyCounter] <= 'Z' ? keyCode - 'A'.charCodeAt(0) : keyCode - 'a'.charCodeAt(0);
+          const reducer = (message[i] <= 'Z') ? 'A'.charCodeAt(0) : 'a'.charCodeAt(0);
+          const reducedChar = message[i].charCodeAt(0) - reducer;
+          const keyedChar = (crypt === 'encrypt') ? (reducedChar + keyShift) % 26 : (reducedChar + (26 - keyShift)) % 26;
+          returnMessage += String.fromCharCode(keyedChar + reducer);
+          keyCounter = (keyCounter + 1) % key.length;
+        } else {
+          returnMessage += message[i];
+        }
+      }
+      return returnMessage;
+    } else {
+      throw("Incorrect key type passed to vigenereCrypt. Expected a string with only letters, received " + key + ".");
+    }
   }
 }
 
--- src/CryptHandler.test.js
+++ src/CryptHandler.test.js
@@ -16,6 +16,7 @@ describe('cryptHandler', () => {
 });
 
 describe('caesar', () => {
+  
   test('returns the same string with key 0', () => {
     const caesarEncryption = CryptHandler.handleCrypt('caesar', 0, 'encrypt', message);
     expect(caesarEncryption).toBe("Fish Sandwich!");
@@ -35,5 +36,38 @@ describe('caesar', () => {
     const caesarEncryption = CryptHandler.handleCrypt('caesar', 1, 'encrypt', 'AAZZ az azz zz');
     expect(caesarEncryption).toBe('BBAA ba baa aa');
   });
+
+  test('throws when passed a non integer', () => {
+    expect(() => {
+      CrytpHandler.handleCrypt('caesar', 'fish', 'encrypt', message);
+    }).toThrow();
+  });
+
+});
+
+describe('vigenere', () => {
+  
+  test('encrypts Fish Sandwich to Giuv Fbnfkvdh! with key bacon', () => {
+    const vigenereEncrypt = CryptHandler.handleCrypt('vigenere', 'bacon', 'encrypt', message);
+    expect(vigenereEncrypt).toBe('Giuv Fbnfkvdh!');
+  });
+
+  test('decrypts Giuv Fbnfkvdh! to Fish Sandwich with key bacon', () => {
+    const vigenereDecrypt = CryptHandler.handleCrypt('vigenere', 'bacon', 'decrypt', 'Giuv Fbnfkvdh!');
+    expect(vigenereDecrypt).toBe(message);
+  });
+
+  test('throws when passed a string with a non letter', () => {
+    expect(() => {
+      CryptHandler.handleCrypt('vigenere', 'fish0', 'encrypt', message);
+    }).toThrow();
+  });
+
+  test('throws when passed a string with a space', () => {
+    expect(() => {
+      CrytpHandler.handleCrypt('vigenere', 'fish and stuff', 'encrypt', message);
+    }).toThrow();
+  });
+
 });
 
