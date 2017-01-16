ab39f0d26449d387333e72b62912aaaa07bb8261
ReidasaurusRex <reidhlewis91@gmail.com>
2017-01-16 09:39:36 -0800

    Added inline-block display to header sub elements

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CryptSelect from './CryptSelect/CryptSelect';
 import CipherSelect from './CipherSelect/CipherSelect';
+import './Header.css'
 
 class Header extends Component {
   constructor(props) {
@@ -17,11 +18,11 @@ class Header extends Component {
   render() {
     var self = this;
     return (
-      <header>
+      <header className='c-header'>
         
-        <h2>{this.props.title}</h2>
+        <h2 className='c-header__title'>{this.props.title}</h2>
 
-        <nav>
+        <nav className='c-header__nav'>
           <CryptSelect handleCryptChange={self.handleCryptChange} currentCrypt={self.props.currentCrypt} />
           <CipherSelect handleCipherChange={self.handleCipherChange} currentCipher={self.props.currentCipher} />
         </nav>
